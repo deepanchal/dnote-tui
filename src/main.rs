25fdb48e25782496abbdea5b15021dd3d663006e
Deep Panchal <deep302001@gmail.com>
2023-07-08 16:09:11 -0500

    feat(main): test out DnoteClient in main fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() -> Result<(), std::io::Error> {
-    println!("Hello, world!");
+mod dnote_lib;
+
+use dnote_lib::*;
+
+fn main() -> Result<(), DnoteClientError> {
+    let client = DnoteClient {};
+    let books = client.view_books()?;
+    println!("Found books {:?}", books);
+
+    let book_name = &books[0].name;
+    let pages = client.view_pages(book_name)?;
+    println!("Found pages in Book::{:?}: {:?}", book_name, pages);
+    
+    let page_id = pages[0].id;
+    let page_info = client.view_page_info(page_id)?;
+    println!("Found page content with id {}:\n {:?}", page_id, page_info);
     Ok(())
 }

68c4af8671f4f40d2fb4b2ef55dc915e27373284
Deep Panchal <deep302001@gmail.com>
2023-07-08 14:16:05 -0500

    feat(lib): implement FromStr trait for DnoteBook to parse string into struct

--- src/dnote_lib.rs
+++ src/dnote_lib.rs
@@ -1,6 +1,16 @@
+use std::str::FromStr;
+
 #[derive(Debug, Clone)]
 pub struct DnoteBook {
     pub name: String,
+}
+
+impl FromStr for DnoteBook {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let name = s.trim().to_string();
+        Ok(DnoteBook { name })
+    }
 }
 
 #[derive(Debug, Clone)]

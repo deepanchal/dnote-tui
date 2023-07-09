3ebf5c4b4d34b31f54111b9a66e62a9194d421f7
Deep Panchal <deep302001@gmail.com>
2023-07-09 09:39:57 -0500

    feat(setup): use rust-tui-template to setup files & mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+/// Dnote Lib
 pub mod dnote_lib;
+
+/// Application.
+pub mod app;
+
+/// Terminal events handler.
+pub mod event;
+
+/// Widget renderer.
+pub mod ui;
+
+/// Terminal user interface.
+pub mod tui;
+
+/// Event handler.
+pub mod handler;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-mod dnote_lib;
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+use dnote_tui::app::{App, AppResult};
+use dnote_tui::event::{Event, EventHandler};
+use dnote_tui::handler::handle_key_events;
+use dnote_tui::tui::Tui;
 
-use dnote_lib::*;
+fn main() -> AppResult<()> {
+    // Create an application.
+    let mut app = App::new();
 
-fn main() -> Result<(), DnoteClientError> {
-    let client = DnoteClient {};
-    let books = client.view_books()?;
-    println!("Found books {:?}", books);
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
 
-    let book_name = &books[0].name;
-    let pages = client.view_pages(book_name)?;
-    println!("Found pages in Book::{:?}: {:?}", book_name, pages);
-    
-    let page_id = pages[0].id;
-    let page_info = client.view_page_info(page_id)?;
-    println!("Found page content with id {}:\n {:?}", page_id, page_info);
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next()? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
+    }
+
+    // Exit the user interface.
+    tui.exit()?;
     Ok(())
 }

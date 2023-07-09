40305e59a7f07de644bcead799f307046910085b
Deep Panchal <deep302001@gmail.com>
2023-07-09 14:43:52 -0500

    feat(handler): add keybindings for navigating through books list

--- src/handler.rs
+++ src/handler.rs
@@ -15,12 +15,10 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
             }
         }
         // Counter handlers
-        KeyCode::Right => {
-            app.increment_counter();
-        }
-        KeyCode::Left => {
-            app.decrement_counter();
-        }
+        KeyCode::Left | KeyCode::Char('h') => app.books.unselect(),
+        KeyCode::Right | KeyCode::Char('l') => app.books.next(),
+        KeyCode::Up | KeyCode::Char('k') => app.books.previous(),
+        KeyCode::Down | KeyCode::Char('j') => app.books.next(),
         // Other handlers you could add here.
         _ => {}
     }

1b7e5f8f525a36e983a94e13d3cb35992040f953
Deep Panchal <deep302001@gmail.com>
2023-07-16 16:56:45 -0500

    feat(handler): handle navigation using selected_chunk & helper methods

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,4 @@
-use crate::app::{App, AppResult};
+use crate::app::{App, AppResult, TuiChunk};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 /// Handles the key events and updates the state of [`App`].
@@ -15,10 +15,32 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
             }
         }
         // Counter handlers
-        KeyCode::Left | KeyCode::Char('h') => app.books.unselect(),
-        KeyCode::Right | KeyCode::Char('l') => app.books.next(),
-        KeyCode::Up | KeyCode::Char('k') => app.books.previous(),
-        KeyCode::Down | KeyCode::Char('j') => app.books.next(),
+        KeyCode::Left | KeyCode::Char('h') => {
+            app.select_prev_chunk();
+            match app.selected_chunk {
+                TuiChunk::BOOKS => {},
+                TuiChunk::PAGES => app.books.previous(),
+                TuiChunk::CONTENT => app.pages.previous(),
+            }
+        }
+        KeyCode::Right | KeyCode::Char('l') => {
+            app.select_next_chunk();
+            match app.selected_chunk {
+                TuiChunk::BOOKS => app.pages.next(),
+                TuiChunk::PAGES => {},
+                _ => {}
+            }
+        }
+        KeyCode::Up | KeyCode::Char('k') => match app.selected_chunk {
+            TuiChunk::BOOKS => app.books.previous(),
+            TuiChunk::PAGES => app.pages.previous(),
+            _ => {}
+        },
+        KeyCode::Down | KeyCode::Char('j') => match app.selected_chunk {
+            TuiChunk::BOOKS => app.books.next(),
+            TuiChunk::PAGES => app.pages.next(),
+            _ => {}
+        },
         // Other handlers you could add here.
         _ => {}
     }

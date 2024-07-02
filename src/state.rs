cdd4c63b7c87fa483ec340cb3e861da211465d04
Deep Panchal <deep302001@gmail.com>
2024-07-01 21:33:42 -0500

    feat(state): add StatefulList struct, update members of State struct
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/state.rs
+++ src/state.rs
@@ -1,12 +1,61 @@
 use color_eyre::eyre::Result;
+use ratatui::widgets::ListState;
 use serde::{Deserialize, Serialize};
 
 use crate::dnote::{DnoteBook, DnotePage};
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    pub fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct State {
-    active_book: Option<DnoteBook>,
-    active_page: Option<DnotePage>,
+    pub books: StatefulList<DnoteBook>,
+    pub pages: StatefulList<DnotePage>,
+    pub page_content: Option<String>,
 }
 
 impl State {

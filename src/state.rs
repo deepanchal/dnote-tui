4cff058e6170463fa9352d024355fa461eae190c
Deep Panchal <deep302001@gmail.com>
2024-07-03 10:06:03 -0500

    test(unit): add unit tests for State struct
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/state.rs
+++ src/state.rs
@@ -102,3 +102,209 @@ impl State {
         self.pages.previous()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn stateful_list_with_items_creates_list_with_given_items() {
+        let items = vec![1, 2, 3];
+        let list = StatefulList::with_items(items.clone());
+        assert_eq!(list.items, items);
+        assert_eq!(list.state.selected(), None);
+    }
+
+    #[test]
+    fn stateful_list_next_wraps_around() {
+        let items = vec![1, 2, 3];
+        let mut list = StatefulList::with_items(items);
+
+        list.next();
+        assert_eq!(list.state.selected(), Some(0));
+
+        list.next();
+        list.next();
+        list.next();
+        assert_eq!(list.state.selected(), Some(2));
+
+        list.next();
+        assert_eq!(list.state.selected(), Some(2));
+    }
+
+    #[test]
+    fn stateful_list_previous_wraps_around() {
+        let items = vec![1, 2, 3];
+        let mut list = StatefulList::with_items(items);
+
+        list.previous();
+        assert_eq!(list.state.selected(), Some(0));
+
+        list.next();
+        list.next();
+        list.previous();
+        assert_eq!(list.state.selected(), Some(1));
+
+        list.previous();
+        assert_eq!(list.state.selected(), Some(0));
+
+        list.previous();
+        assert_eq!(list.state.selected(), Some(0));
+    }
+
+    #[test]
+    fn stateful_list_unselect_clears_selection() {
+        let items = vec![1, 2, 3];
+        let mut list = StatefulList::with_items(items);
+
+        list.next();
+        assert_eq!(list.state.selected(), Some(0));
+
+        list.unselect();
+        assert_eq!(list.state.selected(), None);
+    }
+
+    #[test]
+    fn state_new_creates_default_state() {
+        let state = State::new();
+        assert_eq!(state, State::default());
+    }
+
+    #[test]
+    fn state_get_active_book_returns_selected_book() {
+        let books = vec![
+            DnoteBook {
+                name: String::from("Book1"),
+            },
+            DnoteBook {
+                name: String::from("Book2"),
+            },
+        ];
+        let mut state = State::new();
+        state.books = StatefulList::with_items(books.clone());
+
+        state.books.state.select(Some(1));
+        assert_eq!(state.get_active_book(), Some(books[1].clone()));
+
+        state.books.state.select(Some(0));
+        assert_eq!(state.get_active_book(), Some(books[0].clone()));
+
+        state.books.state.select(None);
+        assert_eq!(state.get_active_book(), None);
+    }
+
+    #[test]
+    fn state_get_active_page_returns_selected_page() {
+        let pages = vec![
+            DnotePage {
+                id: 1,
+                summary: String::from("Page1"),
+            },
+            DnotePage {
+                id: 2,
+                summary: String::from("Page2"),
+            },
+        ];
+        let mut state = State::new();
+        state.pages = StatefulList::with_items(pages.clone());
+
+        state.pages.state.select(Some(1));
+        assert_eq!(state.get_active_page(), Some(pages[1].clone()));
+
+        state.pages.state.select(Some(0));
+        assert_eq!(state.get_active_page(), Some(pages[0].clone()));
+
+        state.pages.state.select(None);
+        assert_eq!(state.get_active_page(), None);
+    }
+
+    #[test]
+    fn state_select_next_book_navigates_correctly() {
+        let books = vec![
+            DnoteBook {
+                name: String::from("Book1"),
+            },
+            DnoteBook {
+                name: String::from("Book2"),
+            },
+        ];
+        let mut state = State::new();
+        state.books = StatefulList::with_items(books);
+
+        state.select_next_book();
+        assert_eq!(state.books.state.selected(), Some(0));
+
+        state.select_next_book();
+        assert_eq!(state.books.state.selected(), Some(1));
+
+        state.select_next_book();
+        assert_eq!(state.books.state.selected(), Some(1));
+    }
+
+    #[test]
+    fn state_select_prev_book_navigates_correctly() {
+        let books = vec![
+            DnoteBook {
+                name: String::from("Book1"),
+            },
+            DnoteBook {
+                name: String::from("Book2"),
+            },
+        ];
+        let mut state = State::new();
+        state.books = StatefulList::with_items(books);
+
+        state.select_prev_book();
+        assert_eq!(state.books.state.selected(), Some(0));
+
+        state.select_prev_book();
+        assert_eq!(state.books.state.selected(), Some(0));
+    }
+
+    #[test]
+    fn state_select_next_page_navigates_correctly() {
+        let pages = vec![
+            DnotePage {
+                id: 1,
+                summary: String::from("Page1"),
+            },
+            DnotePage {
+                id: 2,
+                summary: String::from("Page2"),
+            },
+        ];
+        let mut state = State::new();
+        state.pages = StatefulList::with_items(pages);
+
+        state.select_next_page();
+        assert_eq!(state.pages.state.selected(), Some(0));
+
+        state.select_next_page();
+        assert_eq!(state.pages.state.selected(), Some(1));
+
+        state.select_next_page();
+        assert_eq!(state.pages.state.selected(), Some(1));
+    }
+
+    #[test]
+    fn state_select_prev_page_navigates_correctly() {
+        let pages = vec![
+            DnotePage {
+                id: 1,
+                summary: String::from("Page1"),
+            },
+            DnotePage {
+                id: 2,
+                summary: String::from("Page2"),
+            },
+        ];
+        let mut state = State::new();
+        state.pages = StatefulList::with_items(pages);
+
+        state.select_prev_page();
+        assert_eq!(state.pages.state.selected(), Some(0));
+
+        state.select_prev_page();
+        assert_eq!(state.pages.state.selected(), Some(0));
+    }
+}

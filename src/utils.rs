bc1287c98b66c47a3448556933cd3159f22f2dbb
Deep Panchal <deep302001@gmail.com>
2024-07-02 21:29:10 -0500

    fix(app): fix build issues in initialize_panic_handler util
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -58,12 +58,13 @@ pub fn initialize_panic_handler() -> Result<()> {
         #[cfg(not(debug_assertions))]
         {
             use human_panic::{handle_dump, print_msg, Metadata};
-            let meta = Metadata {
-                version: env!("CARGO_PKG_VERSION").into(),
-                name: env!("CARGO_PKG_NAME").into(),
-                authors: env!("CARGO_PKG_AUTHORS").replace(':', ", ").into(),
-                homepage: env!("CARGO_PKG_HOMEPAGE").into(),
-            };
+            let meta = Metadata::new(
+                env!("CARGO_PKG_NAME"),
+                env!("CARGO_PKG_VERSION"),
+                Some(env!("CARGO_PKG_AUTHORS").replace(':', ", ")),
+                Some(env!("CARGO_PKG_HOMEPAGE")),
+                Some(env!("CARGO_PKG_REPOSITORY")),
+            );
 
             let file_path = handle_dump(&meta, panic_info);
             // prints human-panic message

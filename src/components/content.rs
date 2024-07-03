995e413c11203db1a1c133e1a2dfc94058de708b
Deep Panchal <deep302001@gmail.com>
2024-07-02 20:11:19 -0500

    fix(comps): add title padding in content pane title
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- src/components/content.rs
+++ src/components/content.rs
@@ -56,11 +56,13 @@ impl Component for ContentPane {
 
     fn draw(&mut self, f: &mut Frame<'_>, area: Rect, state: &mut State) -> Result<()> {
         let title = Title::from(" Content ".cyan().bold());
+        let title_padding = Line::from("");
         // let block = Block::default().borders(Borders::ALL).title(title);
         let block = Block::default()
             .borders(Borders::ALL)
             // .padding(Padding::proportional(1))
             .border_set(border::ROUNDED)
+            .title(title_padding.clone().left_aligned())
             .title(title);
         let mut paragraph = Paragraph::new("".to_string()).block(block.clone());
         if let Some(note) = &state.page_content {

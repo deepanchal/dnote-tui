4af35f29dc6554050c678375b10fe6e875f26592
Deep Panchal <deep302001@gmail.com>
2024-09-28 16:52:19 -0500

    fix(build): upgrade build-deps & build.rs
    Signed-off-by: Deep Panchal <deep302001@gmail.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
-use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+use anyhow::Result;
+use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
+    let build = BuildBuilder::all_build()?;
+    let gix = GixBuilder::all_git()?;
+    let cargo = CargoBuilder::all_cargo()?;
     Emitter::default()
-        .add_instructions(&BuildBuilder::all_build()?)?
-        .add_instructions(&Git2Builder::all_git()?)?
-        .emit()?;
-    Ok(())
+        .add_instructions(&build)?
+        .add_instructions(&gix)?
+        .add_instructions(&cargo)?
+        .emit()
 }
